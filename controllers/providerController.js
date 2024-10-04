e28ff063095be8ae6eeafeb519ca3c3096572d74
Viet Tien Dang <vdang074@uottawa.ca>
2024-10-03 21:42:08 -0400

    add provider

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -1,4 +1,5 @@
-const {createProviderService, createProviderServiceService, getListProviderService}= require("../services/ProviderService")
+const {createProviderService, createProviderServiceService, getListProviderService, getProviderService, 
+    getProviderServiceService, updateProviderService, updateProviderServiceService}= require("../services/ProviderService")
 const createProvider = async(req,res) =>{
     const data=req.body
     const result = await createProviderService(data);
@@ -13,4 +14,22 @@ const getListProvider = async(req,res) =>{
     const result = await getListProviderService();
     return res.status(200).json(result)
 }
-module.exports = {createProvider,addProviderService, getListProvider} //export default
+const getProvider = async(req,res) =>{
+    const id=req.params.userid
+    const result = await getProviderService(id);
+    return res.status(200).json(result)
+}
+const getPService = async(req,res) =>{
+    const id=req.params.user_id
+    const result = await getProviderServiceService(id);
+    return res.status(200).json(result)
+}
+const updateProvider = async(req,res)=>{
+    const result = await updateProviderService(req.body)
+    return res.status(200).json(result)
+}
+const updatePService = async(req,res)=>{
+    const result = await updateProviderServiceService(req.body)
+    return res.status(200).json(result)
+}
+module.exports = {createProvider,addProviderService, getListProvider, getProvider, getPService, updateProvider, updatePService} //export default
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {createCustomer,createEmployee,handleLogin, getUser, getAccount, getCustomer, getSpecificCustomer, 
     getSpecificCustomerRequests, getCustomerTransactions, createCompany, getService, addCustomerRequest,updateCustomer,
      getCompany, getRequests,addService, addRecurringRequest, addTransaction, updateCompany} = require('../controllers/userController')
-const {createProvider, addProviderService, getListProvider} = require('../controllers/providerController')
+const {createProvider, addProviderService, getListProvider, getProvider,getPService, updateProvider,updatePService} = require('../controllers/providerController')
 const {reportUser} = require('../controllers/reportController')
 const { auth } = require('../middleware/auth');
 
@@ -59,15 +59,25 @@ routerAPI.get("/getrequests", getRequests)
 
 //GetProvider
 routerAPI.get("/getlistprovider", getListProvider)
+//getProvider by id
+routerAPI.get("/getprovider/:userid", getProvider)
 
+//getProviderService
+routerAPI.get("/getproviderservice/:user_id", getPService)
 
 //UPDATE
 //UpdateCustomer
 routerAPI.put("/updatecustomer",updateCustomer)
 //UpdateCompany
 routerAPI.put("/updatecompany",updateCompany)
-module.exports = routerAPI; //export default
 
+//updateProvider
+routerAPI.put("/updateprovider",updateProvider)
+
+//updateProviderService
+routerAPI.put("/updateproviderservice",updatePService)
 
 //REPORT
 routerAPI.get("/reportuser", reportUser)
+
+module.exports = routerAPI; //export default
